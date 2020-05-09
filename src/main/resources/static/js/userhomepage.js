9bcf26320d9a25f5d4b8323814d14b7ef811a9e6
zjk97 <zhangjake97@gmail.com>
2020-05-09 14:16:07 -0400

    test deployment with 1 sec update

--- src/main/resources/static/js/userhomepage.js
+++ src/main/resources/static/js/userhomepage.js
@@ -248,7 +248,7 @@ class Tile extends React.Component {
 
     initupdate() {
         if (!this.state.updating) {
-            setInterval(this.handleupdate, 5000);
+            setInterval(this.handleupdate, 1000);
             this.setState({updating: true})
         }
     }
